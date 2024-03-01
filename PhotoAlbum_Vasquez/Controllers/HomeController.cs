4ac156287da4de8fca76e27732628489edd96851
marcreann <160214747+marcreann@users.noreply.github.com>
2024-02-29 20:10:53 -0800

    PhotoAlbum_Vasquez

---
+++ PhotoAlbum_Vasquez/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using PhotoAlbum_Vasquez.Models;
+using System.Diagnostics;
+
+namespace PhotoAlbum_Vasquez.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Stats()
+        {
+            return View();
+        }
+        public IActionResult Players()
+        {
+            return View();
+        }
+        public IActionResult Teams()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Leaders()
+        {
+            return View();
+        }
+        public IActionResult HallOfFame()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PhotoAlbum_Vasquez/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhotoAlbum_Vasquez.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
